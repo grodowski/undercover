29fa06434fbb690f99df14f8466f0798797600b5
Bartek Kruszczynski <kruszczyk@gmail.com>
2018-05-14 19:59:23 +0200

    Add tests for Undercover::Formatter

---
+++ spec/formatter_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'undercover'
+require 'pry'
+
+describe Undercover::Formatter do
+  context 'without warnings' do
+    let(:results) { {} }
+
+    it 'returns a message' do
+      formatted = described_class.new(results).to_s
+      puts formatted
+      expect(formatted).to include('No coverage is missing in latest changes')
+    end
+  end
+
+  context 'with warnings' do
+    let(:ast) { Imagen.from_local('spec/fixtures/class.rb') }
+    let(:lcov) do
+      Undercover::LcovParser.parse('spec/fixtures/fixtures.lcov')
+    end
+    let(:coverage) { lcov.source_files['spec/fixtures/class.rb'] }
+    let(:node) { ast.find_all(with_name('BaconClass')).first }
+    let(:result) { Undercover::Result.new(node, coverage, 'class.rb') }
+
+    let(:results) { [result] }
+
+    it 'returns a useful message' do
+      formatted = described_class.new(results).to_s
+      expect(formatted).to include('some methods have no test coverage')
+      expect(formatted).to include('node `BaconClass`')
+      expect(formatted).to include('type: class')
+      expect(formatted).to include('loc: class.rb:3:18')
+      expect(formatted).to include('coverage: 83.33%')
+    end
+  end
+end
