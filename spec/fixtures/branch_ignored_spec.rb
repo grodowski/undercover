c97dbdc40dee89f3f905c3e85e60fe510a935357
Jan Grodowski <jgrodowski@gmail.com>
2025-08-28 09:28:06 +0200

    comment out the mock spec

--- spec/fixtures/branch_ignored_spec.rb
+++ spec/fixtures/branch_ignored_spec.rb
@@ -1,21 +1,24 @@
-require 'rspec'
-require 'simplecov'
+# Commented out, this spec is only used to generate branch_ignored_coverage/branch_ignored_test.json
+# and we don't want to pollute global SimpleCov state in the test suite.
 
-SimpleCov.start do
-  enable_coverage(:branch)
-  add_filter(/^\/spec\//)
-  coverage_dir('branch_ignored_coverage')
-end
+# require 'rspec'
+# require 'simplecov'
 
-require 'undercover/simplecov_formatter'
+# SimpleCov.start do
+#   enable_coverage(:branch)
+#   add_filter(/^\/spec\//)
+#   coverage_dir('branch_ignored_coverage')
+# end
 
-SimpleCov::Formatter::Undercover.output_filename = 'branch_ignored_test.json'
-SimpleCov.formatter = SimpleCov::Formatter::Undercover
+# require 'undercover/simplecov_formatter'
 
-require_relative './branch_ignored'
+# SimpleCov::Formatter::Undercover.output_filename = 'branch_ignored_test.json'
+# SimpleCov.formatter = SimpleCov::Formatter::Undercover
 
-RSpec.describe '#test_branch_ignored' do
-  it 'works' do
-    expect(test_branch_ignored(:arg1)).to eq(:sym1)
-  end
-end
+# require_relative './branch_ignored'
+
+# RSpec.describe '#test_branch_ignored' do
+#   it 'works' do
+#     expect(test_branch_ignored(:arg1)).to eq(:sym1)
+#   end
+# end
